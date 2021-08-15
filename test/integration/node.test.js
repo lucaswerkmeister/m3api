06105bd84b74513436a4f37480e11b1fa53fbe5e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-08-15 23:53:23 +0200

    Add integration test for logging in and editing
    This tests that the cookie jar works and the session is persisted
    between requests on the same session instance. MEDIAWIKI_USERNAME and
    MEDIAWIKI_PASSWORD must be in the environment, either from a .env file
    or directly set as environment variables.

--- test/integration/node.test.js
+++ test/integration/node.test.js
@@ -2,6 +2,7 @@
 
 import NodeSession from '../../node.js';
 import { expect } from 'chai';
+import 'dotenv/config';
 
 describe( 'NodeSession', () => {
 
@@ -29,6 +30,51 @@ describe( 'NodeSession', () => {
 			password: [ 0, 0, 0 ].map( () => ( Math.random() + 1 ).toString( 36 ).slice( 2 ) ).join( '' ),
 		}, 'POST' );
 		expect( response.validatepassword.validity ).to.equal( 'Good' );
+	} );
+
+	it( 'login, edit', async function () {
+		if ( !( 'MEDIAWIKI_USERNAME' in process.env && 'MEDIAWIKI_PASSWORD' in process.env ) ) {
+			return this.skip();
+		}
+		this.timeout( 10000 ); // this test makes a lot of requests
+		const session = new NodeSession( 'https://en.wikipedia.beta.wmflabs.org/w/api.php', {
+			formatversion: 2,
+		} );
+		const { query: { tokens: { logintoken } } } = await session.request( {
+			action: 'query',
+			meta: [ 'tokens' ],
+			type: [ 'login' ],
+		} );
+		const { login: { lgusername: username } } = await session.request( {
+			action: 'login',
+			lgname: process.env.MEDIAWIKI_USERNAME,
+			lgpassword: process.env.MEDIAWIKI_PASSWORD,
+			lgtoken: logintoken,
+		}, 'POST' );
+		const { query: { tokens: { csrftoken } } } = await session.request( {
+			action: 'query',
+			meta: [ 'tokens' ],
+			type: [ 'csrf' ],
+		} );
+		const title = `User:${username}/m3api test`;
+		const text = `Test content (${new Date().toISOString()}).`;
+		await session.request( {
+			action: 'edit',
+			title,
+			text,
+			token: csrftoken,
+			bot: true,
+			assert: 'user',
+		}, 'POST' );
+		const { query: { pages: [ page ] } } = await session.request( {
+			action: 'query',
+			titles: [ title ],
+			prop: [ 'revisions' ],
+			rvprop: [ 'content' ],
+			rvslots: [ 'main' ],
+		} );
+		const { revisions: [ { slots: { main: { content } } } ] } = page;
+		expect( content ).to.equal( text );
 	} );
 
 } );
