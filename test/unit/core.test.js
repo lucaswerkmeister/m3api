1753fbc93346f21eb573fd6214f092c2adf103d5
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-08-15 21:39:41 +0200

    Add unit tests for requestAndContinue
    Amazingly, this seems to actually work without requiring any fixes ^^

--- test/unit/core.test.js
+++ test/unit/core.test.js
@@ -65,6 +65,173 @@ describe( 'Session', () => {
 		} );
 	} );
 
+	describe( 'requestAndContinue', () => {
+
+		it( 'query (GET)', async () => {
+			const firstResponse = {
+				batchcomplete: true,
+				continue: {
+					gapcontinue: '!!',
+					continue: 'gapcontinue||',
+				},
+				query: {
+					pages: [
+						{
+							pageid: 5878274,
+							ns: 0,
+							title: '!',
+						},
+					],
+				},
+			};
+			const secondResponse = {
+				batchcomplete: true,
+				query: {
+					pages: [
+						{
+							pageid: 3632887,
+							ns: 0,
+							title: '!!',
+						},
+					],
+				},
+			};
+			let call = 0;
+			class TestSession extends Session {
+				async internalGet( params ) {
+					const currentCall = ++call;
+					if ( currentCall === 1 ) {
+						expect( params ).to.eql( {
+							action: 'query',
+							generator: 'allpages',
+							gaplimit: '1',
+							format: 'json',
+							formatversion: '2',
+						} );
+						return firstResponse;
+					} else if ( currentCall === 2 ) {
+						expect( params ).to.eql( {
+							action: 'query',
+							generator: 'allpages',
+							gaplimit: '1',
+							gapcontinue: '!!',
+							continue: 'gapcontinue||',
+							format: 'json',
+							formatversion: '2',
+						} );
+						return secondResponse;
+					} else {
+						throw new Error( `Unexpected call #${currentCall}` );
+					}
+				}
+			}
+
+			const session = new TestSession( 'https://en.wikipedia.org/w/api.php', {
+				formatversion: 2,
+			} );
+			const params = {
+				action: 'query',
+				generator: 'allpages',
+				gaplimit: 1,
+			};
+			let iteration = 0;
+			for await ( const response of session.requestAndContinue( params ) ) {
+				const currentIteration = ++iteration;
+				if ( currentIteration === 1 ) {
+					expect( response ).to.eql( firstResponse );
+				} else if ( currentIteration === 2 ) {
+					expect( response ).to.eql( secondResponse );
+				} else {
+					throw new Error( `Unexpected iteration #${currentIteration}` );
+				}
+			}
+
+			expect( call ).to.equal( 2 );
+			expect( iteration ).to.equal( 2 );
+		} );
+
+		it( 'purge (POST)', async () => {
+			const firstResponse = {
+				batchcomplete: true,
+				continue: {
+					gapcontinue: '!!',
+					continue: 'gapcontinue||',
+				},
+				purge: [
+					{
+						ns: 0,
+						title: '!',
+						purged: true,
+					},
+				],
+			};
+			const secondResponse = {
+				batchcomplete: true,
+				purge: [
+					{
+						ns: 0,
+						title: '!!',
+						purged: true,
+					},
+				],
+			};
+			let call = 0;
+			class TestSession extends Session {
+				async internalPost( urlParams, bodyParams ) {
+					expect( urlParams ).to.eql( {} );
+					const currentCall = ++call;
+					if ( currentCall === 1 ) {
+						expect( bodyParams ).to.eql( {
+							action: 'purge',
+							generator: 'allpages',
+							gaplimit: '1',
+							format: 'json',
+							formatversion: '2',
+						} );
+						return firstResponse;
+					} else if ( currentCall === 2 ) {
+						expect( bodyParams ).to.eql( {
+							action: 'purge',
+							generator: 'allpages',
+							gaplimit: '1',
+							gapcontinue: '!!',
+							continue: 'gapcontinue||',
+							format: 'json',
+							formatversion: '2',
+						} );
+						return secondResponse;
+					} else {
+						throw new Error( `Unexpected call #${currentCall}` );
+					}
+				}
+			}
+
+			const session = new TestSession( 'https://en.wikipedia.org/w/api.php', {
+				formatversion: 2,
+			} );
+			const params = {
+				action: 'purge',
+				generator: 'allpages',
+				gaplimit: 1,
+			};
+			let iteration = 0;
+			for await ( const response of session.requestAndContinue( params, 'POST' ) ) {
+				const currentIteration = ++iteration;
+				if ( currentIteration === 1 ) {
+					expect( response ).to.eql( firstResponse );
+				} else if ( currentIteration === 2 ) {
+					expect( response ).to.eql( secondResponse );
+				} else {
+					throw new Error( `Unexpected iteration #${currentIteration}` );
+				}
+			}
+
+			expect( call ).to.equal( 2 );
+			expect( iteration ).to.equal( 2 );
+		} );
+
+	} );
+
 	describe( 'throwErrors', () => {
 
 		it( 'formatversion=1', () => {
